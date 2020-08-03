bdf70acfc4b2536221bc05c976cff5467ec43677
DESKTOP-QJR2767\pralk <smiec>
2020-08-03 12:04:41 +0200

    missing files

---
+++ ResponseHandlers/AdminAddedResponseHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CapsBallCore
+{
+    public class AdminAddedResponseHandler : IResponseHandler
+    {
+        public int ParamsRequiredCount => 0;
+
+        public static event Action AdminAdded;
+
+        public void Handle(ResponsePackage responsePackage)
+        {
+            CachedData.IsAdmin = true; //TODO remve?
+            AdminAdded?.Invoke();
+        }
+    }
+}
---
+++ ResponseHandlers/GameStartedResponseHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CapsBallCore
+{
+    public class GameStartedEventArgs
+    {
+        public string StarterNick { get; private set; }
+
+        public GameStartedEventArgs(string starterNick) => StarterNick = starterNick;
+    }
+
+    public class GameStartedResponseHandler : IResponseHandler
+    {
+        public int ParamsRequiredCount => 1;
+
+        public static event EventHandler<GameStartedEventArgs> GameStarted;
+
+        public void Handle(ResponsePackage responsePackage)
+        {
+            string starterNick = responsePackage.Parameters[0];
+            GameStarted?.Invoke(this, new GameStartedEventArgs(starterNick));
+        }
+    }
+}
